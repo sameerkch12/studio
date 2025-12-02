5b10197f4c8a77db0ae122ae695fb7e38e51a4e9
Licxo Rental <licxorental@gmail.com>
2025-12-02 15:22:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6671892799-d2b12",
+  "appId": "1:1013781784762:web:a45db8a1565e203795ba83",
+  "apiKey": "AIzaSyBWf2r4F9pBHnTfQJzAxRst7bGrLSdutfI",
+  "authDomain": "studio-6671892799-d2b12.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1013781784762"
+};
